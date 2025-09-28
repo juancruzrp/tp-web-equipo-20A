3750ff4317fb9944e48659ac0c54a531637cd53c
ivana0996 <ivanaescobar96@hotmail.com>
2025-09-28 14:28:55 -0300

    dominio y negocio copypaste

---
+++ dominio/Articulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public double Precio { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+    }
+}
